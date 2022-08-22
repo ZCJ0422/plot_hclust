0139c98aaf636a08df0de260bed82402460e4619
ZCJ0422 <959264868@qq.com>
2022-08-22 14:32:06 +0800

    update

--- plot_hclust.R
+++ plot_hclust.R
@@ -7,5 +7,5 @@
 # gheatmap(p,iris[,1:4],width = .1,colnames_position = "top",colnames_offset_y = 25,colnames_angle = 90,hjust = 1)
 
 
-p <- dist(iris[,-5])%>% hclust() %>% ggtree()
+p <- dist(iris[,-5]) %>% hclust() %>% ggtree()
 gheatmap(p,iris[,1:4],width = .1,colnames_position = "top",colnames_offset_y = 25,colnames_angle = 90,hjust = 1)+scale_colour_subtree(4)
