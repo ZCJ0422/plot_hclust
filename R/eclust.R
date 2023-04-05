039d1c07e0d0d6dff33b6422a6d75eff61a7f1e3
chuanjiezhang <959264868@qq.com>
2023-04-05 11:50:24 +0800

    update

--- R/eclust.R
+++ R/eclust.R
@@ -1,54 +1,46 @@
 library(eclust)
 library(SummarizedExperiment) |> suppressPackageStartupMessages()
 dat <- data.frame(matrix(rnorm(100), 20))
-dat2 <- data.frame(matrix(rnorm(100), 20))
-dat3 <- data.frame(matrix(rnorm(1), 5))
 se <- SummarizedExperiment(assay = dat)
 ec <- eclust(se, direction = "both")
-assay(ec, 2) <- dat2
-metadata(ec@colData) <- dat3
 
 library(tidytree)
 library(tibble)
-f <- function(object,bycol = TRUE,assay = 1,metada = FALSE) {
-  da <- assay(object, assay) |> as_tibble() |> rownames_to_column(var = "label")
+f <- function(ec,bycol = TRUE,assay = 1,longer=FALSE,col,name,value,metada = FALSE) {
+  
+  da <- assay(ec, assay) |> as_tibble() |> rownames_to_column(var = "label")
+  if (longer){
+    da <- pivot_longer(da,cols = col,names_to = name,values_to = value)
+  }
   
   if (bycol) {
-    ph <- colhclust(object) |> as.phylo()
-    ext <- colData(object) |> as_tibble() |> rownames_to_column(var = "label")
-    
-    if (metada){
-      md <- object@colData@metadata 
-    }
+    ph <- colhclust(ec) |> as.phylo()
+    md <- colData(ec)
   } else {
-    ph <- rowhclust(object) |> as.phylo()
-    ext <- rowData(object) |> as_tibble() |> rownames_to_column(var = "label")
-    if (metada){
-      md <- object@elementMetadata@metadata 
-    }
+    ph <- rowhclust(ec) |> as.phylo()
+    md <- rowData(ec)
   }
-
-  tda <- treedata(phylo = ph) |> left_join(da) |> left_join(ext)
   
-  if(metada){
-    md <- rownames_to_column(md,var = "label")
-    tda <- left_join(tda,md)
+  tda <- treedata(phylo = ph) |> left_join(da)
+  
+  if (metada) {
+    md <- as_tibble(md) |> rownames_to_column(var = "label")
+    tda <- left_join(tda, md)
   }
+  
   return(tda)
 }
-
 library(ggtree)
-tree <- f(ec,metada = TRUE)
-tree1 <- f(ec,bycol = FALSE)
+tree <- f(ec, metada = TRUE,longer = TRUE,col = c(paste0('X',seq(5))),name = "sampleName",value = "count")
+tree1 <- f(ec, bycol = FALSE)
 ggtree(tree@phylo) + geom_tiplab()
 ggtree(tree1@phylo) + geom_tiplab()
 
-
-# autoplot.eclust <- function(object, bycol = TRUE, ...) {
+# autoplot.eclust <- function(ec, bycol = TRUE, ...) {
 #     if (bycol) {
-#         hc <- colhclust(object)
+#         hc <- colhclust(ec)
 #     } else {
-#         hc <- rowhclust(object)
+#         hc <- rowhclust(ec)
 #     }
 #     autoplot.hclust(hc, ...)
 # }
