73a5afc871a43e1fb78186ce841d7e8821e5935a
chuanjiezhang <959264868@qq.com>
2023-04-06 23:05:44 +0800

    update

--- R/eclust.R
+++ R/eclust.R
@@ -3,35 +3,44 @@ library(SummarizedExperiment) |> suppressPackageStartupMessages()
 dat <- data.frame(matrix(rnorm(100), 20))
 se <- SummarizedExperiment(assay = dat)
 ec <- eclust(se, direction = "both")
+assayNames(se) <- 'counts'
 
 library(tidytree)
 library(tibble)
-f <- function(ec,bycol = TRUE,assay = 1,longer=FALSE,col,name,value,metada = FALSE) {
-  
-  da <- assay(ec, assay) |> as_tibble() |> rownames_to_column(var = "label")
-  if (longer){
-    da <- pivot_longer(da,cols = col,names_to = name,values_to = value)
-  }
-  
-  if (bycol) {
-    ph <- colhclust(ec) |> as.phylo()
-    md <- colData(ec)
-  } else {
-    ph <- rowhclust(ec) |> as.phylo()
-    md <- rowData(ec)
-  }
-  
-  tda <- treedata(phylo = ph) |> left_join(da)
-  
-  if (metada) {
-    md <- as_tibble(md) |> rownames_to_column(var = "label")
-    tda <- left_join(tda, md)
-  }
-  
-  return(tda)
+library(tidyr)
+f <- function(ec,bycol = TRUE,assay = 1,longer = FALSE,metada = FALSE) {
+    da <- assay(ec, assay) |> data.frame() |> rownames_to_column(var = "label")
+    name <- assayNames(se)[assay]
+    
+    if (bycol) {
+      ph <- colhclust(ec) |> as.phylo()
+      md <- colData(ec)
+      if (longer) {
+        da <-pivot_longer(da,cols = !'label',names_to = 'sample',values_to = name
+          ) |> nest(counts = c(sample, name))
+      }
+      
+    } else {
+      ph <- rowhclust(ec) |> as.phylo()
+      md <- rowData(ec)
+      if (longer) {
+        da <-pivot_longer(da,cols = !'label',names_to = 'feature',values_to = name
+          ) |> nest(counts = c(feature, name))
+      }
+    }
+    
+    tda <- treedata(phylo = ph) |> left_join(da)
+    
+    if (metada) {
+      md <- as_tibble(md) |> rownames_to_column(var = "label")
+      tda <- left_join(tda, md)
+    }
+    
+    return(tda)
 }
+
 library(ggtree)
-tree <- f(ec, metada = TRUE,longer = TRUE,col = c(paste0('X',seq(5))),name = "sampleName",value = "count")
+tree <- f(ec, metada = TRUE, longer = TRUE)
 tree1 <- f(ec, bycol = FALSE)
 ggtree(tree@phylo) + geom_tiplab()
 ggtree(tree1@phylo) + geom_tiplab()
